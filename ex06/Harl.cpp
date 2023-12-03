c97754c895e5fb84d0170ffe80464fafbe2b64b6
Shota Nemoto <snemoto@c1r20s11.42tokyo.jp>
2023-12-03 14:55:43 +0900

    1203-5

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: snemoto <snemoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 12:48:30 by snemoto           #+#    #+#             */
-/*   Updated: 2023/12/03 14:34:00 by snemoto          ###   ########.fr       */
+/*   Updated: 2023/12/03 14:54:27 by snemoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,10 @@ void	Harl::complain(std::string level)
 	{
 		case 0:
 			debug();
-			break ;
 		case 1:
 			info();
-			break ;
 		case 2:
 			warning();
-			break ;
 		case 3:
 			error();
 			break ;
@@ -54,22 +51,26 @@ void	Harl::debug(void)
 {
 	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+	std::cout << "\n";
 }
 
 void	Harl::info(void)
 {
 	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+	std::cout << "\n";
 }
 
 void	Harl::warning(void)
 {
 	std::cout << "[ WARNING ]\n";
 	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n";
+	std::cout << "\n";
 }
 
 void	Harl::error(void)
 {
 	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+	std::cout << "\n";
 }
